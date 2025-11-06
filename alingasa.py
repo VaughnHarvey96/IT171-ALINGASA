4ae8dd6fca27f0a9ac94a0a78ae3b5ceaa019f13
VaughnHarvey96 <vaughnharvey.alingasa@gmail.com>
2025-11-07 02:53:53 +0800

    Update alingasa.py

--- alingasa.py
+++ alingasa.py
@@ -1,5 +1,5 @@
-player_x = 0
-player_y = 0
+player_dudut = 0
+player_harvey = 0
 treasure_x = 5
 treasure_y = 3
 game_running = True
@@ -7,16 +7,16 @@ print(f"Find the treasure at ({treasure_x}, {treasure_y})!")
 while game_running:
   move = input("Enter move (up/down/left/right): ")
   if move == "w":
-    player_y += 1
+    player_harvey += 1
   if move == "s":
-    player_y -= 1
+    player_harvey -= 1
   if move == "d":
-    player_x += 1
+    player_dudut += 1
   if move == "a":
-    player_x -= 1
+    player_dudut -= 1
   else:
     print("invalid move!)  
-  print(f"Player position: ({player_x}, {player_y})")
+  print(f"Player position: ({player_dudut}, {player_harvey})")
   if player_x == treasure_x and player_y == treasure_y:
     print("You Found the treasure!")
     game_running = False
